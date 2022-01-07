d5fa1172c1d660775630fa523589fead5b02fd2d
AndreLima001 <a2019127839@isec.pt>
2022-01-07 18:08:09 +0000

    admin inicial features, delete user

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Airbnb_PWEB.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Airbnb_PWEB.Controllers
+{
+    public class RolesController : Controller
+    {
+
+        RoleManager<IdentityRole> roleManager;
+        UserManager<ApplicationUser> userManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View(roleManager.Roles.ToList());
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+
+            var role = roleManager.FindByIdAsync(id);
+
+            if (role != null) 
+            {
+                var listaDeUtilizadores = await userManager.GetUsersInRoleAsync(role.Result.Name);
+                if (listaDeUtilizadores != null) { 
+                    RoleViewModel roleViewModel = new RoleViewModel
+                    {
+                        RoleId = role.Result.Id,
+                        Nome = role.Result.Name,
+                        Utilizadores = listaDeUtilizadores
+                    };
+                    return View(roleViewModel);
+                }
+                return NotFound();
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit()
+        {
+            return View("Edit");
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await userManager.DeleteAsync(user);
+            return View("Details");
+        }
+
+        // POST: Reservations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(ApplicationUser user)
+        {
+
+            await userManager.DeleteAsync(user);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+    }
+}
---
+++ Models/RoleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airbnb_PWEB.Models
+{
+    public class RoleViewModel
+    {
+        public string RoleId { get; set; }
+
+        public string Nome { get; set; }
+
+        public IEnumerable<ApplicationUser> Utilizadores { get; set; }
+    }
+}
