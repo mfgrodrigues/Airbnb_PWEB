81fb80f47acf0abe0f80c5f0016f66bfa07e1e63
mfgrodrigues <frodrigues.mgomes@gmail.com>
2022-01-19 11:54:14 +0000

    Add DB Script

---
+++ Data/Migrations/20220119114059_insertScript.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.IO;
+
+namespace Airbnb_PWEB.Data.Migrations
+{
+    public partial class insertScript : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string sqlContent = File.ReadAllText("./Data/Initial-Data/script.sql");
+            migrationBuilder.Sql(sqlContent);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
